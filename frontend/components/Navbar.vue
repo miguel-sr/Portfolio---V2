5b2dae58d33c6cbabe5ec5ebd0c277e842f67937
Miguel S. Ramos <miguelsramos458@gmail.com>
2022-12-27 15:39:27 -0300

    :sparkles: feat: Add new brand

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,11 +1,22 @@
 <template>
   <nav class="navbar navbar-expand-lg w-100 position-fixed">
     <div class="container-fluid px-4 py-2">
-      <nuxt-link id="navbar-brand" to="/">Miguel S. Ramos</nuxt-link>
+      <nuxt-link id="navbar-brand" class="d-flex align-items-center" to="/">
+        Miguel S. Ramos
+      </nuxt-link>
       <div id="navbarSupportedContent" class="collapse navbar-collapse">
         <ul class="navbar-nav align-items-center ms-auto mb-2 mb-lg-0">
           <li class="nav-item hover-effect">
             <nuxt-link class="nav-link" to="/">Home</nuxt-link>
+          </li>
+          <li class="nav-item hover-effect">
+            <nuxt-link class="nav-link" to="/">About me</nuxt-link>
+          </li>
+          <li class="nav-item hover-effect">
+            <nuxt-link class="nav-link" to="/">Skills</nuxt-link>
+          </li>
+          <li class="nav-item hover-effect">
+            <nuxt-link class="nav-link" to="/">Work</nuxt-link>
           </li>
         </ul>
       </div>
@@ -42,6 +53,16 @@ nav #navbar-brand {
   font-weight: 800;
   font-size: 2rem;
   text-decoration: none;
+}
+
+nav #navbar-brand::before {
+  content: "";
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  margin-right: 15px;
+  background: url(@/assets/brand.svg) no-repeat;
+  background-position: center;
 }
 
 nav .nav-item a {
