a032fbdbda851324ce636179b6e968785be5b8e0
Miguel S. Ramos <miguelsramos458@gmail.com>
2022-12-27 09:34:46 -0300

    :tada: init: Initialize nuxt project

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+}
