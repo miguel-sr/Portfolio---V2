03737e36a4a6f6f63d2fb4b99ff0afa67948b77b
Miguel S. Ramos <miguelsramos458@gmail.com>
2022-12-28 00:22:49 -0300

    :sparkles: feat: Create admin/index.vue

---
+++ frontend/pages/admin/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Navbar />
+    <section class="d-flex flex-column justify-content-center">
+      <div class="me-auto ms-auto text-center">
+        <h2>Painel Administrativo</h2>
+        <div class="d-flex flex-column justify-content-center">
+          <nuxt-link class="m-3 w-75 mx-auto" to="/admin">
+            <Button text="Skills" />
+          </nuxt-link>
+          <nuxt-link class="m-3 w-75 mx-auto" to="/">
+            <Button text="Projects" />
+          </nuxt-link>
+        </div>
+      </div>
+    </section>
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
+
+<style scoped>
+h2 {
+  font-weight: 800;
+  color: var(--light-yellow);
+}
+
+section {
+  min-height: 100vh;
+}
+</style>
